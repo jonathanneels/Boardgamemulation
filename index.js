cd068407393ebc3b1bd1986f1ccaa240c40319ce
jonathanneels <jonathanneels@hotmail.com>
2020-05-03 14:26:17 +0200

    Add files via upload
    Working 3D boardgame simulation with multiplayer. Can always be made better (sockets, nicer get/post handlers), but for now it's a working prototype. Play Backgammon till D&D. 
    
    Note; it's possible to upload own projects to the server but not to add files.

---
+++ index.js
@@ -0,0 +1,456 @@
+// zie package.json for header size https://github.com/cloudfoundry/nodejs-buildpack/issues/176 => atm unused
+const https = require('https');
+//const http = require('http');
+ const fs = require('fs');
+var os = require( 'os' );
+const path = require('path');
+
+const { parse } = require('querystring');
+
+const options = {
+  key: fs.readFileSync('testkey.pem'),
+  cert: fs.readFileSync('test.crt')//cert.pem
+};
+
+ var Chatdict = {
+  "guid-unique1Test" :  ["bla","ble"],
+  "guid-unique2Test" :  [ "dsfsfsmkj"],
+ }
+ var gameAlivedict={
+	   "codename" :  "2020-01-01 20:29:20"
+ }
+  var playerdict = { 
+  "guid-unique1Test" :  ["Game Master" ],
+ }
+ var playerturndict = {
+  "guid-unique1Test" :   "Game Master"  
+ }
+var Projectsdict = {
+  "guid-unique1Test" : "htmlcontent",
+  "guid-unique2Test" :  "<html>",
+ };
+var Meshesdict = {
+  "guid-unique1Test" : "mesh:a",
+  "guid-unique2Test" :  "mesh:b",
+ };
+ 
+const directoryPath = path.join(__dirname, 'static');
+
+ var port = 8002;
+ var ip= "127.0.0.1";
+ require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+ // console.log('addr: '+add);
+  ip = add; // if netwerk allows it - Windows  Firewall - https://stackoverflow.com/questions/5489956/how-could-others-on-a-local-network-access-my-nodejs-app-while-its-running-on/5490033
+ 	console.log("HTTPS server started at https://"+ip+":" + port.toString());
+	
+	launchServer();
+	setInterval(serverTimerActions, 1000);
+
+});
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+function removeA(arr) {//REF: https://stackoverflow.com/questions/3954438/how-to-remove-item-from-array-by-value
+    var what, a = arguments, L = a.length, ax;
+    while (L > 1 && arr.length) {
+        what = a[--L];
+        while ((ax= arr.indexOf(what)) !== -1) {
+            arr.splice(ax, 1);
+        }
+    }
+    return arr;
+}
+
+function dateTimeNow()//REF: https://usefulangle.com/post/187/nodejs-get-date-time
+{
+	
+	let ts = Date.now();
+
+let date_ob = new Date(ts);
+let date = date_ob.getDate();
+let month = date_ob.getMonth() + 1;
+let year = date_ob.getFullYear();
+let hours = date_ob.getHours();
+ let minutes = date_ob.getMinutes();
+ let seconds = date_ob.getSeconds();
+
+ return(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
+
+}
+
+var olderThanRemoveDateTime; 
+var CountBeforeUpdateOlderThanCheckSeconds= 750;//1500 = 25 min
+var CurrentSecondBeforeUpdateOlderThanCheckSeconds=749;
+var CountUpdatePublicGamesList= 30;
+var CurrentSecondUpdatePublicGamesList=0;
+var PublicGamesList=[];
+var PublicGamesByAdminCodeList=[];
+
+function IsJsonString(str) {
+    try {
+        JSON.parse(str);
+		
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+
+function serverTimerActions() {
+   
+  CurrentSecondBeforeUpdateOlderThanCheckSeconds+=1;
+  CurrentSecondUpdatePublicGamesList+=1;
+   if(CurrentSecondUpdatePublicGamesList >= CountUpdatePublicGamesList)
+  {PublicGamesList=[];PublicGamesByAdminCodeList=[];
+	  for (var key in Projectsdict) {
+ 
+ if (IsJsonString(Projectsdict[key]) && Projectsdict.hasOwnProperty(key)) {
+ 			if(JSON.parse(Projectsdict[key]).publicserver === true)
+		{var convdata = JSON.parse(Projectsdict[key]);
+			PublicGamesList.push( convdata.projectname);
+			PublicGamesByAdminCodeList.push(convdata.AdminCode);
+			}}
+			}
+ 		
+	  CurrentSecondUpdatePublicGamesList= 0;
+//	  console.log("Public games: " + PublicGamesList.join("**"))
+	  }
+	  
+	  
+  if(CurrentSecondBeforeUpdateOlderThanCheckSeconds >= CountBeforeUpdateOlderThanCheckSeconds)
+  {
+			// older than handler
+			for (var key in gameAlivedict) {
+    // check if the property/key is defined in the object itself, not in parent
+    if (gameAlivedict.hasOwnProperty(key)) { 
+if(Date.parse(gameAlivedict[key]) < Date.parse(olderThanRemoveDateTime)){
+ 				 	delete gameAlivedict[key]
+					 	delete Chatdict[key];
+				 	delete playerdict[key];
+				 	delete playerturndict[key];
+					 delete Projectsdict[key];
+				 	delete Meshesdict[key];}
+ 
+    }
+}
+ 
+			//then;
+ 
+ olderThanRemoveDateTime= dateTimeNow()
+	  	    console.log('Will delete unresponsive games older than; '+olderThanRemoveDateTime); 
+	  CurrentSecondBeforeUpdateOlderThanCheckSeconds= 0; 
+  }
+}
+
+function launchServer(){
+https.createServer(options, function (req, res) { 
+  if (req.url === '/') {//REF: https://stackoverflow.com/questions/4720343/loading-basic-html-in-node-js
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream('index.html').pipe(res);
+    } 
+	else  if (req.url.includes( '/adminpage')) {//else  if (req.url === '/adminpage') { //ref: https://stackoverflow.com/questions/37991995/passing-a-variable-from-node-js-to-html
+        res.writeHead(200, {'Content-Type': 'text/html'});
+		fs.readFile("playfield.html", "utf8", function(err, data) { 
+              res.end(data + "<label id='lblAdminCode'>"+uuidv4() +"</label><label id='lblisAdmin'>true</label>"); 
+			  });			  }
+			  	else  if (req.url.includes( '/addorgetplayer')) {  
+		var playerandadminhandler= req.url.replace('/addorgetplayer','');
+		var admincode = playerandadminhandler.split("?")[0].trim();
+				var player = playerandadminhandler.split("?")[1].trim();
+
+			var array=[];
+			 if(playerdict[admincode]===undefined){
+				 if(player.trim() ==""){
+					 					res.end("");	return;	
+ 					 
+				 }
+				 playerdict[admincode]=[];
+				 }
+				else{			  array = playerdict[admincode];
+				if(array.includes(player) || player.trim() =="" ){ // player.trim() =="" => if it's just getting the list
+					res.writeHead(200);
+					res.end(playerdict[admincode].join("\n"));	return;	
+
+				}
+				
+				}
+			array.push(player);
+			playerdict[admincode]=array;  
+				        res.writeHead(200); 
+res.end(playerdict[admincode].join("\n"));		
+   }
+   else  if (req.url.includes( '/playerturn')) {  
+		var playerandadminhandler= req.url.replace('/playerturn','');
+		var admincode = playerandadminhandler.split("?")[0].trim();
+				var player = playerandadminhandler.split("?")[1].trim();
+ 			 if(playerdict[admincode]===undefined){
+ 					 					res.end("No players found.");	return;	 
+
+				 }
+				else{		 
+				var array = playerdict[admincode];
+				for (i = 0; i < array.length; i++) {
+					//console.log(array[i] +"--"+ player);
+					if(array[i] === player)
+					{						if(i === array.length -1)
+						{
+							playerturndict[admincode]=array[0];
+						}
+						else
+						{ 
+						 playerturndict[admincode]=array[i+1];
+
+						}
+						break;	
+					}
+
+						} 
+  					res.writeHead(200);
+					res.end(playerturndict[admincode]);	return;	 
+				
+				}  
+			 playerturndict[admincode] = player; 
+				res.writeHead(200); 
+				res.end(playerturndict[admincode]);		// no other players than the current..
+   }
+   else  if (req.url.includes( '/removeplayer')) {  
+		var playerandadminhandler= req.url.replace('/removeplayer','').trim();
+		var playeradminparts=playerandadminhandler.split("?");
+		var admincode = playeradminparts[0].trim();
+				var player = playeradminparts[1].trim();
+
+			var array=[];
+			 if(playerdict[admincode]===undefined){
+ 					 					res.end("Game not found.");	return;	
+
+				 }
+				else{			  array = playerdict[admincode];
+			 
+					if(!array.includes(player) || player.trim() =="" ){	res.end("Player not found");	return;	}
+
+				}
+ 			removeA(array, player); 
+			playerdict[admincode]=array;  
+				        res.writeHead(200); 
+res.end("Removed: "+ player);		
+   }   else  if (req.url.includes( '/activeplayer')) {
+	   		var admincode= req.url.replace('/activeplayer','');
+ 			 if(playerturndict[admincode]===undefined){
+ 					 					res.end("Game not found.");	return;	
+
+				 }
+
+	res.writeHead(200); 
+				res.end(playerturndict[admincode]);		   
+ } 
+ else  if (req.url.includes( '/adminAliveCheck')) {
+	   		var admincode= req.url.replace('/adminAliveCheck','').trim();
+ 			 if(Projectsdict[admincode]===undefined){
+ 					 					res.end("Game not found.");	return;	
+
+				 }
+gameAlivedict[admincode] = dateTimeNow();
+
+	res.writeHead(200); 
+				res.end(gameAlivedict[admincode]);		   
+ } 
+  else  if (req.url===( '/PublicGamesList')) { 
+	res.writeHead(200); 
+				res.end(PublicGamesList.join("\n"));		   
+ }   else  if (req.url===( '/PublicGamesListByAdmincode')) { 
+	res.writeHead(200); 
+				res.end(PublicGamesByAdminCodeList.join("\n"));		   
+ } 
+
+	else  if (req.url.includes( '/project')) {  
+		var admincodeclienthandler= req.url.replace('/project','').trim();
+		if(Projectsdict[admincodeclienthandler]===undefined || playerdict[admincodeclienthandler]===undefined ){res.end("room does not exist");return;}
+		var projectdata = Projectsdict[admincodeclienthandler];
+		if(JSON.parse(projectdata).maxplayer-1 < playerdict[admincodeclienthandler].length)
+		{			
+			 res.end("Game is full, all places are occupied. Sorry...");return;
+		
+		}
+		fs.readFile("playfield.html", "utf8", function(err, data) {         res.writeHead(200);
+
+              res.end(data + "<label id='lblAdminCode'>"+admincodeclienthandler +"</label><label id='lblisAdmin'>false</label><label id='lblProjectContent'>"+projectdata+"</label>"); 
+			  });			
+   }
+	else  if (req.url.includes( '/getproject')) {  
+		var admincodeclienthandler= req.url.replace('/getproject','').trim();
+		if(Projectsdict[admincodeclienthandler]===undefined){res.end("room does not exist");}
+		var projectdata = Projectsdict[admincodeclienthandler];
+		        res.writeHead(200); 
+              res.end(projectdata); 
+    } 	   
+	else  if (req.url === '/postProject') {//REF: https://itnext.io/how-to-handle-the-post-request-body-in-node-js-without-using-a-framework-cd2038b93190
+
+	if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+    req.on('end', () => {
+      //  console.log(
+         //   parse(body)
+			          //   JSON.parse(body)  
+
+      //  );
+		   /*console.log(
+            JSON.parse(body).email 
+        );*/
+		try{var test=JSON.parse(body)}catch(err){        res.end('corrupt JSON-data');return;}
+		Projectsdict[JSON.parse(body).AdminCode.trim()]=body
+		        res.writeHead(200);
+        res.end('ok');
+    });
+}
+
+    } 
+	else  if (req.url === '/postMeshes') {  
+
+	if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+    req.on('end', () => {
+     //   console.log(
+ 			        //    JSON.parse(body)  
+
+   //     );
+ 		//playerturndict[admincode]=  JSON.parse(body)[0].playerturn;
+		
+		 	if(Meshesdict[JSON.parse(body)[0].AdminCode.trim()] === undefined || Date.parse(JSON.parse(Meshesdict[JSON.parse(body)[0].AdminCode].trim())[0].posttime) < Date.parse(JSON.parse(body)[0].posttime)){
+	 
+		try{var test=JSON.parse(body)}catch(err){        res.end('corrupt JSON-data');return;}
+
+	 Meshesdict[JSON.parse(body)[0].AdminCode.trim()]=body;
+	 	res.end('ok');}else{console.log("old data :"+ JSON.parse(Meshesdict[JSON.parse(body)[0].AdminCode.trim()])[0].posttime +"-"+ JSON.parse(body)[0].posttime); res.end("old data :"+ JSON.parse(Meshesdict[JSON.parse(body)[0].AdminCode])[0].posttime +"-"+ JSON.parse(body)[0].posttime);}
+     });
+}
+
+    } else  if (req.url.includes( '/getMeshes')) {  
+				var admincode = req.url.replace('/getMeshes','').trim();
+								//console.log(admincode); 
+			//	console.log(Meshesdict[admincode]);
+			// Meshesdict[admincode][0]=		playerturndict[admincode] ;
+
+  		res.writeHead(200); 
+		 if(Meshesdict[admincode]===undefined || Meshesdict[admincode]===[] ){res.end("NONE");}
+		else{ res.end(Meshesdict[admincode] );}
+  } 
+		else  if (req.url.includes( '/chat')) {  
+				var feedback = req.url.replace('/chat','').trim();
+ 				var editedFeedList=  feedback.split("?");
+				var admincode = editedFeedList[0];
+		if (editedFeedList[1] !== undefined  && editedFeedList[1] !== "undefined"  ){
+							var chatcontentwithoutcode = editedFeedList[1].replace((admincode+"?").trim(),""); 
+			var array=[];
+			 if(Chatdict[admincode]===undefined){Chatdict[admincode]=[];}
+				else{			  array = Chatdict[admincode];}
+			array.push(chatcontentwithoutcode);
+			Chatdict[admincode]=array;
+        }
+		res.writeHead(200);  console.log("chat:"+admincode+"----"+chatcontentwithoutcode);
+		 if(Chatdict[admincode]===undefined || Chatdict[admincode]===[] ){res.end("");}
+		else{ res.end(Chatdict[admincode].join("\n"));}
+  } 
+  
+  else  if (req.url === ( '/getCustomProjectForShare')) {   
+  			
+	if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+        body += decodeURI( chunk).toString();
+    });
+    req.on('end', () => { 
+
+
+			var feedback = body.trim();//console.log(feedback);
+ 				var projectFeed=feedback.split("?");
+				var name =  (projectFeed[0]);
+				var content =  projectFeed[1].trim();
+				
+				if(!name.includes(".bgem")){  res.end("wrong project type (.bgem required).");return;}
+
+  fs.writeFile("static/custom_fan_examples/" + name, content, function (err) {
+  if (err) { res.end(err);console.log(err); return;}
+
+  res.writeHead(200); 
+  res.end("Project posted. Thank you");
+       });       });
+
+}
+
+ 
+
+
+     }
+	else  if (req.url.includes( '/directoryitems')) { 
+	var flist=[]; 
+fs.readdir(directoryPath+req.url.replace('/directoryitems',''), function (err, files) {//REF: https://medium.com/stackfame/get-list-of-all-files-in-a-directory-in-node-js-befd31677ec5
+    //handling error
+    if (err) {
+        return res.end('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach 
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        flist.push(file); 
+     });
+ res.end(flist.join("<br>"));}); 
+  } 	   
+  else if (req.url.includes( '/list')){
+	//  console.log(req.url);
+	  var fileList= [];
+	  fs.readdir(("."+req.url.replace("/list","/").trim()), (err, files) => { //REF: https://stackoverflow.com/questions/2727167/how-do-you-get-a-list-of-the-names-of-all-files-present-in-a-directory-in-node-j
+	    files.forEach(file => { try{
+			if(fs.statSync("."+req.url.replace("/list","/").trim()+"/"+file).isDirectory()){// REF: https://www.technicalkeeda.com/nodejs-tutorials/how-to-check-if-path-is-file-or-directory-using-nodejs
+				console.log("dir:"+file);
+				fileList.push("<a href='"+req.url+'/'+file+"'>"+file + "</a><br>");
+			} 
+				
+			else{
+  //  console.log(file);
+ //fileList.push("<a href='https://"+  req.url.replace("/list",ip+":"+port )+"/"+file+"'>"+file + "</a><br>"); 
+  fileList.push("<a href='"+  req.url.replace("/list","/GETFILE" )+"/"+file+"'>"+file + "</a><br>");}
+
+  }catch(err){ 
+    //console.log("File not Found :("); // prevents server crash
+	  return false; 
+  }});  
+res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(fileList.join("")); }); 
+  } else if (req.url .includes( '/GETFILE') )
+  {
+	  fs.readFile(__dirname + req.url.replace("GETFILE","") , function (err,data) {   
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+	  
+  }
+  else{  
+  fs.readFile(__dirname + req.url, function (err,data) {//REF:https://stackoverflow.com/questions/16333790/node-js-quick-file-server-static-files-over-http
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+  
+  }
+  
+}).listen(port,"0.0.0.0");}
---
+++ static/js/LAUNCH.js
@@ -0,0 +1,126 @@
+	//	if(isWithPhysics){
+	        scene.enablePhysics(); //} for walls
+ 
+var allowedgizmoMeshes =[];
+	  		 scene.meshes.forEach(function(m) { 
+			 if(m.id === "board"){return true;}else{allowedgizmoMeshes.push(m);}
+});
+gizmoManager.attachableMeshes = allowedgizmoMeshes;
+
+/////////////////////:///important running and starting: //////
+	$( document ).ready(function() {
+	groundObject();  
+
+			$.get(" /directoryitems/images/board", function( data ) {
+var boards = data.split("<br>");
+for (i = 0; i < boards.length; i++) { 
+if(boards[i]==="blanco.png"){continue;}
+$('#selBoard').append($('<option>', {value:boards[i], text:boards[i]}));
+}
+//load after previous get has been completed;
+			$.get(" /directoryitems/images/textures", function( data ) {
+var texs = data.split("<br>");
+for (i = 0; i < texs.length; i++) { 
+if(texs[i] == "blanco.png"){continue;}
+$('#selObjTexture').append($('<option>', {value:texs[i], text:texs[i]}));
+$('#selObjTexturesideB').append($('<option>', {value:texs[i], text:texs[i]}));
+
+	}
+	
+				$.get(" /directoryitems/audio/sounds", function( data ) {
+var texs = data.split("<br>");
+for (i = 0; i < texs.length; i++) { 
+
+$('#selEndTurn').append($('<option>', {value:texs[i], text:texs[i]}));
+$('#selPieceRelease').append($('<option>', {value:texs[i], text:texs[i]}));
+
+	}
+				$.get(" /directoryitems/audio/music", function( data ) {
+var texs = data.split("<br>");
+for (i = 0; i < texs.length; i++) { 
+
+$('#selBackgroundMusic').append($('<option>', {value:texs[i], text:texs[i]}));
+ 
+	}
+});
+});
+	
+	
+	
+	
+});
+ 
+});
+admincode =$("#lblAdminCode").text();
+  $("#lbladmincode").text("Connection code: " + admincode  );
+
+if($("#lblisAdmin").text() === "true"){ 
+isAdmin=true; isActivePlayer= true;
+pname=userId=playerturn="Game Master";$("#txtProjectName").val(admincode); $("#lblPlayerTurn").text("Player turn: " + userId  );
+
+if((window.location.href).indexOf("?") > -1)//Ex.:https://stackoverflow.com/questions/13669472/window-location-indexof-not-working-in-javascript &  https://192.168.1.2:8002/adminpage?static/examples/Project_3d8bdbec-5482-4bdf-a4e1-f7eecd644b0d.bgem
+{ 
+var prepropjLink ="/"+window.location.href.split("?")[1];
+    $.get(prepropjLink, function(contents) {//Ex (same domain): https://192.168.1.2:8002/adminpage?static/examples/Project_7fdf1794-b80e-4953-9ac9-4852aa0cff52.bgem
+ //console.log(contents);
+var convertedProjectToJson=JSON.parse(contents);
+ //console.log( convertedProjectToJson);
+ 	  	var strScene = JSON.stringify(convertedProjectToJson);  
+handleJSONsaveFileByNewObjects(strScene); 
+    },'text');
+
+}
+
+$("#chatboxrespons").val("%0A=> Launch game; check top-right 'offline',%0Await 10 secs(green).%0A%0AShare the connection-code\n(click/see center page).%0A%0A");
+SendchatMessage();
+
+}
+else{
+$(".admincontrol").hide();
+$("#btnAdminToggle").hide();
+
+isAdmin=false;
+isActivePlayer= false;
+isMeshesGetAllowed= false;
+var randomname="player_" + getRndInteger(0,10000);
+  pname = prompt("Your playername?\nP.S.:Choose the same name as last if you a rejoining a game.", randomname);
+
+if (pname == null || pname.trim() == "") {
+ // console.log("User cancelled the prompt.");
+ userId=  pname=randomname ;//"anonymous"
+} 
+
+else {
+ userId=pname;
+ // name has been set
+}
+
+$("#chatboxrespons").val("=> entered the game.");
+SendchatMessage();
+
+// load client here!!
+var convertedProjectToJson=JSON.parse($("#lblProjectContent").text());
+//$("#lblAdminCode").remove();
+console.log( convertedProjectToJson);
+//BABYLON.SceneLoader.Load("test", convertedProjectToJson, engine, scene);
+	//setTimeout(function(){
+	  	var strScene = JSON.stringify(convertedProjectToJson); // https://www.babylonjs-playground.com/#558RAP#6
+handleJSONsaveFileByNewObjects(strScene);
+$("#lblProjectContent").remove();
+$("#lblisAdmin").remove();
+
+		//console.log(convertedProjectToJson.meshes[6]);
+	//	BABYLON.SceneLoader.ImportMesh('', '', "data:"+JSON.stringify(convertedProjectToJson.meshes[6]), scene) 
+	//}, 3000);
+	     document.getElementById("chLaunchServer").checked = true;
+		$("#lblOnOffStatus").html(  '<span style="color:green"> Online</span>');
+ }
+
+ 
+ 		$.get("addorgetplayer"+admincode+"?"+ userId, function( data ) {
+			countPostGetMeshesAction=0;
+		isMeshesGetAllowed= true;
+
+ // added player to list
+});
+});
